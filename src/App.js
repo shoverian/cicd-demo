6bea582b962dd8fed5074e70122aad905307a10f
shoverian <shover.kyle@gmail.com>
2021-09-20 19:02:20 -0400

    Updated layout to resemble auto quote.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 // import React, { useState, useEffect } from 'react';
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './vfb-logo.png';
 import './App.css';
 import Menu from './components/menu/Menu';
+import Footer from './components/footer/Footer';
+import Panel from './components/panel/Panel';
+import Faqs from './components/faqs/Faqs';
 
 const App = () => {
 //   const [message, setMessage] = useState('...loading')
@@ -21,9 +24,25 @@ const App = () => {
 
   return (
     <div className="App">
-      <div>
-        <Menu/>
+      <div className="row">
+        <div>
+          <Menu />
+        </div>
+        <div className="container">
+          <div className="row">
+            {/* <a href="https://www.vafb.com/"><img src={logo} alt="" class="logo" /></a> */}
+          </div>
+          <div class="row">
+            <div className="col-9">
+              <Panel />
+            </div>
+            <div className="col-3">
+              <Faqs />
+            </div>
+          </div> {/* /row */}
+        </div> {/* /container */}
       </div>
+      <Footer />
     </div>
   );
 }
